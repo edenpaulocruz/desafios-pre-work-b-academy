e5fd2c61c1ef605c94ad6968a6d35d90b920f90c
edenpaulocruz <edenpaulo@gmail.com>
2021-08-22 20:47:02 -0300

    Finalização da manipulação de input de nome

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -2,13 +2,11 @@ const inputName = document.querySelector('[data-js="name"]')
 const prepositions = ["de", "da", "do", "dos"]
 
 inputName.addEventListener('input', (event) => {
-  const arrayInputName = event.target.value
-    .split(' ')
-    .map(item => {
-      return prepositions.includes(item)
-        ? item.toLowerCase()
-        : item.charAt(0).toUpperCase() + item.slice(1)
-  })
-  console.log(arrayInputName)
-  event.target.value = arrayInputName.join(' ')
+  const arrayInputName = event.target.value.toLowerCase().split(' ')
+
+  event.target.value = arrayInputName.map(item => {
+    return prepositions.includes(item)
+      ? item
+      : item.charAt(0).toUpperCase() + item.slice(1)
+  }).join(' ')
 })
