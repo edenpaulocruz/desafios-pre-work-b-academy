ac09cb5fb787671260b9e3c4a68a6b2cee6fecfc
edenpaulocruz <edenpaulo@gmail.com>
2021-08-22 21:23:24 -0300

    Novas melhorias no estilo, WIP criação de seleção de cores

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -10,3 +10,22 @@ inputName.addEventListener('input', (event) => {
       : item.charAt(0).toUpperCase() + item.slice(1)
   }).join(' ')
 })
+
+const form = document.querySelector('[data-js="form')
+const select = document.createElement('select')
+const colors = ["#ffffff", "#000000", "#ff0000", "#00ff00", "#0000ff"]
+
+function createOption(color) {
+  const option = document.createElement('option')
+  option.value = color
+  option.textContent = color
+  return option
+}
+
+colors.forEach(color => {
+  const option = createOption(color)
+  select.appendChild(option)
+})
+
+select.classList.add('select')
+form.appendChild(select)
