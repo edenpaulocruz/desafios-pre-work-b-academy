b5f4e54d5b242406b6c1dad193719ddd71682ca2
edenpaulocruz <edenpaulo@gmail.com>
2021-09-12 01:24:54 -0300

    Inserindo dados dos carros numa nova linha da tabela

--- dia3/src/cars.js
+++ dia3/src/cars.js
@@ -5,25 +5,56 @@ const getFormElement = (event) => (elementName) => {
   return event.target.elements[elementName]
 }
 
+const typeOfElements = {
+  image: insertImage,
+  text: insertText,
+  color: insertColor
+}
+
+function insertImage (value) {
+  const td = document.createElement('td')
+  const img = document.createElement('img')
+  img.src = value
+  img.style.width = '200px'
+  td.appendChild(img)
+  return td
+}
+
+function insertText (value) {
+  const td = document.createElement('td')
+  td.textContent = value
+  return td
+}
+
+function insertColor (value) {
+  const td = document.createElement('td')
+  const div = document.createElement('div')
+  div.style.width = '50px',
+  div.style.height = '50px',
+  div.style.borderRadius = '50%',
+  div.style.backgroundColor = value
+  div.style.border = '1px solid #000'
+  td.appendChild(div)
+  return td
+}
+
 carForm.addEventListener('submit', (event) => {
   event.preventDefault()
   const getElement = getFormElement(event)
 
   const elements = [
-    { type: 'image', value: getElement('image-url').value },
-    { type: 'text', value: getElement('brand-model').value },
+    { type: 'image', value: getElement('image').value },
+    { type: 'text', value: getElement('brand').value },
     { type: 'text', value: getElement('year').value},
     { type: 'text', value: getElement('plate').value},
     { type: 'color', value: getElement('color').value}
   ]
 
-  console.log(elements)
-})
+  const tr = document.createElement('tr')
+  elements.forEach(element => {
+    const td = typeOfElements[element.type](element.value)
+    tr.appendChild(td)
+  })
 
-// function printElementNames(form) {
-//   Array.from(form.elements).forEach(element => {
-//     if (element.type !== 'submit') {
-//       console.log(`${element.name}: ${element.value}`)
-//     }
-//   })
-// }
+  carTable.appendChild(tr)
+})
