d28c7616365222eb27678c3dba43855f3d7b78f2
edenpaulocruz <edenpaulo@gmail.com>
2021-09-17 01:36:14 -0300

    Dia 4 - Exibindo mensagem na tabela quando não há carros cadastrados

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,1 +1,22 @@
 import './style.css'
+
+const url = 'http://localhost:3333/cars'
+const carTable = document.querySelector('[data-js="car-table"')
+
+fetch(url)
+  .then(result => result.json())
+  .then(result => {
+    const cars = result
+    if (cars.length === 0) {
+      const tr = document.createElement('tr')
+      const td = document.createElement('td')
+      td.textContent = 'Nenhum carro encontrado'
+      td.colSpan = 5;
+      tr.appendChild(td)
+      carTable.appendChild(tr)
+      return
+    }
+    return cars.map(function (car) {
+      console.log(car)
+    })
+  })
